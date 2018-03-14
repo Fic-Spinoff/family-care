8d55794e1b57253d6bdd66d28731e6c864bcb3b5
Carlos Bouzón García <carlos.bouzon.garcia@gmail.com>
2018-03-14 16:12:16 +0100

    Design VIP container activity
    The purpose of this commit is to design and implement the VIP
    container activity with a mock behavior. In order to debug the
    interface, the Activity logic make use of Toast to generate messages.
    
    * Thanks to gonzalo for correcting typos and color :P

---
+++ app/src/main/java/es/udc/apm/familycare/VipContainerActivity.java
@@ -0,0 +1,45 @@
+package es.udc.apm.familycare;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class VipContainerActivity extends AppCompatActivity {
+
+    CharSequence text = null;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_zones:
+                    text = "Zones Button pressed!";
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_members:
+                    text = "Members Button pressed!";
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_link:
+                    text = "Link Button pressed!";
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vip_container);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_zones);
+    }
+
+}
