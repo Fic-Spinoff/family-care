bbc71f126b0242c0060e42900cc8ad64343fc5ae
Gonzalo Garcia Arce <gonzalo.nom.es@gmail.com>
2018-03-15 23:19:27 +0100

    Actividad de configuración, sin enlazar. Creado por Diego, problemas con git hago yo el commit

---
+++ app/src/main/java/es/udc/apm/familycare/ConfActivity.java
@@ -0,0 +1,14 @@
+package es.udc.apm.familycare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ConfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conf);
+    }
+
+}
