a19a6796f8d000a373533e682e1c75ff2184b175
Gonzalo Garcia Arce <gonzalo.nom.es@gmail.com>
2018-03-15 22:20:38 +0100

    Creada Splash activity y Login Activity Se ha creado una pantalla onboard que sirve como login. Nuevas imÃgenes, icono de la app, etc.

---
+++ app/src/main/java/es/udc/apm/familycare/SplashActivity.java
@@ -0,0 +1,24 @@
+package es.udc.apm.familycare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 800);
+    }
+}
