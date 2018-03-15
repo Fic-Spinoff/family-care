9d696b21cc857dab9adfbf538a5296962cd9bcf1
Iagocanalejas <iagocanalejas@gmail.com>
2018-03-15 11:52:44 +0100

    Trabajando en la activity de terminos y condiciones

---
+++ app/src/main/java/es/udc/apm/familycare/ActivityTermsAndConditions.java
@@ -0,0 +1,15 @@
+package es.udc.apm.familycare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ActivityTermsAndConditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_conditions);
+    }
+
+}
