cb407742e5c4205ea8579ff384c718daf6647d29
Iago Santos <iagocanalejas@gmail.com>
2018-03-15 21:55:17 +0100

    Activity Terminos y Condiciones (#8)
    * Trabajando en la activity de terminos y condiciones
    
    * Añadidas acciones a los botones y texto real
    
    * Cambios en estilo y enlazadas actividades

--- app/src/main/java/es/udc/apm/familycare/RoleActivity.java
+++ app/src/main/java/es/udc/apm/familycare/RoleActivity.java
@@ -1,14 +1,16 @@
 package es.udc.apm.familycare;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class RoleActivity extends AppCompatActivity {
+
+    public final static  String KEY_ROLE = "KEY_ROLE";
+    public final static  String ROLE_VIP = "ROLE_VIP";
+    public final static  String ROLE_GUARD = "ROLE_GUARD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,33 +20,21 @@ public class RoleActivity extends AppCompatActivity {
         final Button btnGuard = findViewById(R.id.btnCardGuard);
         btnGuard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                onClickRolVigilante(v);
+                Intent intent = new Intent(RoleActivity.this, TermsActivity.class);
+                intent.putExtra(KEY_ROLE, ROLE_GUARD);
+                startActivity(intent);
+                finish();
             }
         });
 
         final Button btnVip = findViewById(R.id.btnCardVip);
         btnVip.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                onClickRolVigilado(v);
+                Intent intent = new Intent(RoleActivity.this, TermsActivity.class);
+                intent.putExtra(KEY_ROLE, ROLE_VIP);
+                startActivity(intent);
+                finish();
             }
         });
-    }
-
-    public void onClickRolVigilante(View v){
-        Context context = getApplicationContext();
-        CharSequence text = getString(R.string.text_toast_vigilante);
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
-    public void onClickRolVigilado(View v){
-        Context context = getApplicationContext();
-        CharSequence text = getString(R.string.text_toast_vigilado);
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
     }
 }
