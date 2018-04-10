8e8beb0f04aa54131da4855ce86a28fd356240d5
Iagocanalejas <iagocanalejas@gmail.com>
2018-04-10 11:30:38 +0200

    WIP

--- app/src/main/java/es/udc/apm/familycare/GuardActivity.java
+++ app/src/main/java/es/udc/apm/familycare/GuardActivity.java
@@ -1,15 +1,12 @@
 package es.udc.apm.familycare;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
