4afce6579021905570517893cac8991b3e120118
Christel González Canales <kirisame@gmail.com>
2018-04-15 21:05:58 +0200

    Login con firebase
    - Asignado proyecto Firebase al proyecto y añadido fichero .json asociado
    - Nuevas dependencias en built.gradle del proyecto (relacionadas con Firebase y auth de Play Services)
    - La clase LoginActivity ahora permite al usuario elegir la cuenta Google y realiza login comunicándose con Firebase

--- app/src/main/java/es/udc/apm/familycare/utils/Constants.java
+++ app/src/main/java/es/udc/apm/familycare/utils/Constants.java
@@ -7,5 +7,6 @@ public class Constants {
     public static final String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.INTERNET};
+    public static final String FIREBASE_URL = "https://family-care-4df72.firebaseio.com/";
 
 }
