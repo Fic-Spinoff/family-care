6980912be15fa89ed9dd6e894485311bf548db72
Carlos Bouzón García <carlos.bouzon.garcia@gmail.com>
2018-04-26 01:15:55 +0200

    Make use of Geofencing to draw zones
    https://github.com/Fic-Spinoff/family-care/issues/27
    
    It was posible draw circles on map without behaviour. It must be
    possible to control if a person enters or exits from one of the drawed
    circles.
    
    * Make use of geofencing.
    * CustomMapFragment refactores.
    * Location and Geofencing as services.
    
    Refactor Geofence service

---
+++ app/src/main/java/es/udc/apm/familycare/maps/GeolocationService.java
@@ -0,0 +1,94 @@
+package es.udc.apm.familycare.maps;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class GeolocationService extends Service {
+
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 5;
+    private static String TAG = "GeolocationService";
+
+    protected FusedLocationProviderClient mFusedLocationClient;
+    protected LocationRequest mLocationRequest;
+    protected LocationCallback mLocationCallback;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getBaseContext());
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+            }
+        };
+
+        createLocationRequest();
+
+        startLocationUpdates();
+        Log.i(TAG, "Service Created");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        Log.i(TAG, "Service Started");
+        return START_NOT_STICKY;
+    }
+
+
+    private void createLocationRequest() {
+
+        mLocationRequest = LocationRequest.create();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                null /* Looper */);
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopLocationUpdates();
+        super.onDestroy();
+    }
+}
