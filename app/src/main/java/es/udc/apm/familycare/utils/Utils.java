0e061e80282d2078e1efe8064a9033c0e2f26396
Gonzalo Garcia Arce <gonzalo.nom.es@gmail.com>
2018-04-27 00:20:36 +0200

    Borrar código comentado

--- app/src/main/java/es/udc/apm/familycare/utils/Utils.java
+++ app/src/main/java/es/udc/apm/familycare/utils/Utils.java
@@ -12,16 +12,6 @@ public class Utils {
         mContext = con;
     }
 
-// --Commented out by Inspection START (22/04/2018 19:26):
-//    /**
-//     * Encode user email to use it as a Firebase key (Firebase does not allow "." in the key name)
-//     * Encoded email is also used as "userEmail", list and item "owner" value
-//     */
-//    public static String encodeEmail(String userEmail) {
-//        return userEmail.replace(".", ",");
-//    }
-// --Commented out by Inspection STOP (22/04/2018 19:26)
-
     //This is a method to Check if the device internet connection is currently on
     public boolean isNetworkAvailable() {
 
