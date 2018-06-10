9c99c89f1c53a56acaa3bad2ed6ba55e899d007d
Gonzalo Garcia Arce <gonzalo.nom.es@gmail.com>
2018-06-11 01:25:00 +0200

    Eliminar Bluetooth

--- app/src/main/java/es/udc/apm/familycare/utils/Constants.java
+++ app/src/main/java/es/udc/apm/familycare/utils/Constants.java
@@ -5,8 +5,7 @@ import android.Manifest;
 public class Constants {
 
     public static final String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH,
-            /*Manifest.permission.BLUETOOTH_ADMIN, */Manifest.permission.INTERNET};
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
 
     public static final String PREFS_USER = "Prefs.User";
     public static final String PREFS_USER_UID = "Prefs.User.UID";
