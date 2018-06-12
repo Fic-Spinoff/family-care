c38f698e2c6d538ed2d36184a356f74aa2e1e0ac
Gonzalo Garcia Arce <gonzalo.nom.es@gmail.com>
2018-06-12 02:07:33 +0200

    Cambiados strings

--- app/src/main/java/es/udc/apm/familycare/receiver/BatteryLevelReceiver.java
+++ app/src/main/java/es/udc/apm/familycare/receiver/BatteryLevelReceiver.java
@@ -4,24 +4,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
-import android.widget.Toast;
-
-import es.udc.apm.familycare.R;
 
 public class BatteryLevelReceiver extends BroadcastReceiver {
+
+    /**
+     * Receives Battery low events
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
-        int level, scale;
-        float totallevel = 0;
+        if (intent.getAction() == null || !Intent.ACTION_BATTERY_LOW.equals(intent.getAction())) {
+            return;
+        }
+
         if(intent.getExtras() != null) {
-            level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
             if ((level >= 0) && (scale > 0)) {
-                totallevel = (level*100)/scale;
-                if (totallevel <= 15) {
-                    String message = context.getString(R.string.battery_low) + ": " + totallevel + "%";
-                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-                }
+                int currentLevel = (level * 100) / scale;
+                // TODO Event
             }
         }
     }
